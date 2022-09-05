3dda3273fca44aa8b2ae66f2932c4cec10673fd6
zhoudd <328704089@qq.com>
2022-09-05 15:13:18 +0800

    feat: 高德地图组件文件

---
+++ src/components/a-map/index.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+export default {
+  name: 'PsrVue3AMap'
+}
+</script>
+<script setup lang="ts">
+
+const props = defineProps<{
+  message: string
+}>()
+
+const emits = defineEmits<{
+  (e: 'my-event', msg: string): void
+}>()
+
+</script>
+
+<template>
+  <div class="title" @click.stop="$emit('my-event',message)">
+    PSR AMap {{ message }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.title {
+  color: red;
+}
+</style>

a7bddb613de3512de2fb0659ea4f7c4ceab813b5
zhoudd <328704089@qq.com>
2022-09-07 11:35:19 +0800

    feat: 支持自定义未聚合marker

--- src/components/a-map/MarkerCluster.vue
+++ src/components/a-map/MarkerCluster.vue
@@ -1,4 +1,11 @@
 <template>
+  <div style="display:none;">
+    <div v-for="markerDatum in clusterDataWithId" class="ct-marker-content" :class="`marker-${markerDatum.markerId}`" ref="customMarkerRef">
+      <div class="ct-marker-content-inner">
+        <slot name="marker" :data="markerDatum"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,7 +14,7 @@ export default {
 }
 </script>
 <script setup lang="ts">
-import {onUnmounted, shallowRef, ShallowRef, watch, watchEffect} from "vue";
+import {computed, onUnmounted, ref, Ref, shallowRef, ShallowRef, watch, watchEffect} from "vue";
 
 const props = withDefaults(defineProps<{
   markerMap?: AMap.Map
@@ -16,35 +23,54 @@ const props = withDefaults(defineProps<{
   clusterMaxZoom?: number
   clusterAverageCenter?: boolean
   clusterStyles?: AMap.MarkerCluster.Style[]
-  clusterRenderClusterMarker?: (data: AMap.MarkerCluster.RenderClusterMarkerData) => void
-  clusterRenderMarker?: (data: AMap.MarkerCluster.RenderMarkerData) => void
+  clusterCustomMarker?: boolean
 }>(), {
   clusterGridSize: 60,
   clusterMaxZoom: 18,
   clusterAverageCenter: true,
-
+  clusterCustomMarker: false
 })
+const clusterDataWithId = computed(() => {
+  const _clusterDataWithId: AMap.MarkerCluster.DataOptions[] = []
+  for (let i = 0; i < props.clusterData.length; i++) {
+    const clusterDatum = props.clusterData[i];
+    _clusterDataWithId.push({
+      ...clusterDatum,
+      markerId: i
+    })
+  }
+  return _clusterDataWithId
+})
+
+const customMarkerRef: Ref<HTMLElement[] | undefined> = ref()
+
 const markerCluster: ShallowRef<AMap.MarkerCluster | undefined> = shallowRef()
 watch(() => props.markerMap, (map) => {
   if (map) {
     map.plugin(['AMap.MarkerCluster'], () => {
       markerCluster.value = new AMap.MarkerCluster(map,
-          props.clusterData,
+          [],
           {
             gridSize: props.clusterGridSize,
             maxZoom: props.clusterMaxZoom,
             averageCenter: props.clusterAverageCenter,
             styles: props.clusterStyles,
-            renderMarker: props.clusterRenderMarker,
-            renderClusterMarker: props.clusterRenderClusterMarker
+            renderMarker: (data: AMap.MarkerCluster.RenderMarkerData) => {
+              if (props.clusterCustomMarker) {
+                const customMarker = customMarkerRef.value?.find(el => el.className === `ct-marker-content marker-${data.data[0].markerId}`)
+                if (customMarker) {
+                  data.marker.setContent(customMarker)
+                }
+              }
+            }
           })
     })
   }
 }, {immediate: true})
 
 watchEffect(() => {
-  if (props.markerMap && props.clusterData) {
-    markerCluster.value?.setData(props.clusterData)
+  if (props.markerMap && clusterDataWithId.value) {
+    markerCluster.value?.setData(clusterDataWithId.value)
   }
 })
 
@@ -56,12 +82,12 @@ onUnmounted(() => {
 </script>
 
 <style scoped lang="scss">
-.ct-content {
+.ct-marker-content {
   width: 0;
   height: 0;
 }
 
-.ct-content-inner {
+.ct-marker-content-inner {
   width: fit-content;
   height: fit-content;
   transform: translateX(-50%) translateY(-100%);

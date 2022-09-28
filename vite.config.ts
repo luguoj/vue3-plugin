a6514d7141973841204ca78a7d6314c2033b0e72
zhoudd <328704089@qq.com>
2022-09-28 16:54:22 +0800

    fix: 打包文件名修订

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'PsrAMap',
-      fileName: 'vue3-plugin-visual'
+      fileName: 'vue3-plugin-echarts'
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖

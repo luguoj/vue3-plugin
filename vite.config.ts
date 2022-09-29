35dcde17817055d28f4185661854ac86bae172e1
zhoudd <328704089@qq.com>
2022-09-29 10:24:28 +0800

    fix: 库名修订

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'PsrAMap',
+      name: 'PsrEcharts',
       fileName: 'vue3-plugin-echarts'
     },
     rollupOptions: {

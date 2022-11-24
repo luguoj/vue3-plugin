76b674e42634b33ab334d9affb355c3f41e28cc3
zhoudd <328704089@qq.com>
2022-11-24 15:52:12 +0800

    feat: 工具栏容器样例

--- src/example/App.vue
+++ src/example/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import Buttons from "./Buttons.vue";
 import Dropdown from "./Dropdown.vue";
-import Services from "./Services.vue";</script>
-
+import Services from "./Services.vue";
+import Containers from "./Containers.vue"</script>
 <template>
+  <h1>Buttons</h1>
   <Buttons/>
+  <h1>Dropdown</h1>
   <Dropdown/>
+  <h1>Containers</h1>
+  <Containers/>
+  <h1>Services</h1>
   <Services/>
 </template>
 
---
+++ src/example/Containers.vue
@@ -0,0 +1,15 @@
+<template>
+  <psr-toolbar>
+    <el-button>button1</el-button>
+    <el-button>button2</el-button>
+  </psr-toolbar>
+</template>
+
+<script setup lang="ts">
+import {PsrToolbar} from "../package"
+import {ElButton} from "element-plus"
+import 'element-plus/es/components/button/style/css'</script>
+
+<style scoped>
+
+</style>

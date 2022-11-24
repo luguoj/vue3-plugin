f9ab3c6158fa0e26351cb290263d32854f1ff50c
zhoudd <328704089@qq.com>
2022-11-24 15:34:45 +0800

    feat: 滚轮横向滚动样例

--- src/example/App.vue
+++ src/example/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import Buttons from "./Buttons.vue";
-import Dropdown from "./Dropdown.vue";</script>
-
+import Dropdown from "./Dropdown.vue";
+import Services from "./Services.vue";</script>
 
 <template>
   <Buttons/>
   <Dropdown/>
+  <Services/>
 </template>
 
 <style scoped>
---
+++ src/example/Services.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {shallowRef, watch} from "vue";
+import {ElScrollbar, ScrollbarInstance} from "element-plus"
+import "element-plus/es/components/scrollbar/style/css"
+import {handleHorizontalScrollingOnWheel} from "../package"
+
+const scrollbarRef = shallowRef<ScrollbarInstance>()
+
+watch(scrollbarRef, scrollbar => {
+  if (scrollbar) {
+    handleHorizontalScrollingOnWheel(scrollbar)
+  }
+})
+</script>
+
+<template>
+  <h3>V scrolling</h3>
+  <div style="width: 100px;height: 200px;border:var(--el-border);">
+    <el-scrollbar always>
+      <div style="width: 10px;word-break: break-all">
+        asdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasd
+      </div>
+    </el-scrollbar>
+  </div>
+  <h3>H scrolling</h3>
+  <div style="width: 200px;height: 100px;border:var(--el-border);">
+    <el-scrollbar always ref="scrollbarRef">
+      <div>
+        asdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasd
+      </div>
+    </el-scrollbar>
+  </div>
+</template>
+
+<style scoped>
+</style>

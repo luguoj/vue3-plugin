5484bad8628ab2983a564d794fcf9a0d8bedec38
zhoudd <328704089@qq.com>
2022-11-25 08:18:22 +0800

    feat: 创建更新表单对话框样例

--- src/example/App.vue
+++ src/example/App.vue
@@ -2,7 +2,8 @@
 import Buttons from "./Buttons.vue";
 import Dropdown from "./Dropdown.vue";
 import Services from "./Services.vue";
-import Containers from "./Containers.vue"</script>
+import Containers from "./Containers.vue"
+import Dialogs from "./Dialogs.vue";</script>
 <template>
   <h1>Buttons</h1>
   <Buttons/>
@@ -12,6 +13,8 @@ import Containers from "./Containers.vue"</script>
   <Containers/>
   <h1>Services</h1>
   <Services/>
+  <h1>Dialogs</h1>
+  <Dialogs/>
 </template>
 
 <style scoped>

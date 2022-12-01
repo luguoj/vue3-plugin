c8add8a0a8524f1d0ca8f00db953e453dd5ef946
zhoudd <328704089@qq.com>
2022-12-01 14:57:11 +0800

    feat: 消息清单组件样例

--- src/example/App.vue
+++ src/example/App.vue
@@ -1,23 +1,65 @@
 <script setup lang="ts">
-
-import {PsrPortalMessage} from "../package";
+import {ElContainer, ElHeader, ElMain} from "element-plus"
+import "element-plus/es/components/container/style/css"
+import "element-plus/es/components/header/style/css"
+import "element-plus/es/components/main/style/css"
+import {PsrMessageList, PsrPortalMessage} from "../package";
 
 const msg = PsrPortalMessage.useMessage()
-msg.debug("test-debug%o", {
-  data: [{a: 1}],
-})
-msg.success("test-info", {
-  data: [{a: 1}],
-  console: true
-})
-msg.error("test-err", {
-  data: [{a: 1}],
-  console: true
-})
 
+function clear() {
+  msg.clear()
+}
+
+function info() {
+  msg.info("test-info", {
+    data: [{a: 1}],
+    console: true
+  })
+}
+
+function debug() {
+  msg.debug("test-debug%o", {
+    data: [{a: 1}],
+  })
+}
+
+function error() {
+  msg.error("test-err", {
+    data: [{a: 1}],
+    console: true
+  })
+}
+
+function success() {
+  msg.success("test-info", {
+    data: [{a: 1}],
+    console: true
+  })
+}
+
+function warn() {
+  msg.warn("test-war", {
+    data: [{a: 1}],
+    console: true
+  })
+}
 </script>
 
 <template>
+  <el-container style="height: 100%;">
+    <el-header>
+      <button @click="clear">clear</button>
+      <button @click="info">info</button>
+      <button @click="error">error</button>
+      <button @click="success">success</button>
+      <button @click="debug">debug</button>
+      <button @click="warn">warn</button>
+    </el-header>
+    <el-main>
+      <PsrMessageList/>
+    </el-main>
+  </el-container>
 </template>
 
 <style scoped>

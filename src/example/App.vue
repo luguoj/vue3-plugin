80d8fbcf1875cd2cc119409c5e7fd81143e615ef
zhoudd <328704089@qq.com>
2022-12-01 13:05:57 +0800

    feat: 样例

--- src/example/App.vue
+++ src/example/App.vue
@@ -1,4 +1,20 @@
 <script setup lang="ts">
+
+import {PsrPortalMessage} from "../package";
+
+const msg = PsrPortalMessage.useMessage()
+msg.debug("test-debug%o", {
+  data: [{a: 1}],
+})
+msg.success("test-info", {
+  data: [{a: 1}],
+  console: true
+})
+msg.error("test-err", {
+  data: [{a: 1}],
+  console: true
+})
+
 </script>
 
 <template>
--- src/example/main.ts
+++ src/example/main.ts
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
+import {createApp, inject} from 'vue'
 import './style.css'
 import App from './App.vue'
+import {LogService, PsrPortalMessage} from "../package";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use({
+    install(app) {
+        app.provide<LogService>("logService", (message) => {
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    console.log('message saved:%o', message)
+                    resolve(true)
+                }, 1000)
+            })
+        })
+    }
+})
+app.use(new PsrPortalMessage({
+    debugging: true,
+    logger: () => inject<LogService>("logService")!
+}))
+app.mount('#app')

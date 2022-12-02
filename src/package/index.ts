a76cbfda929fd03a67cb1b8a3fc0b181d267ce67
zhoudd <328704089@qq.com>
2022-12-02 16:36:43 +0800

    fix: 没有导出pinia扩展接口

--- src/package/index.ts
+++ src/package/index.ts
@@ -1,2 +1,16 @@
+import {StateTree} from "pinia";
+import {PsrPiniaPersistTypes} from "./types/PsrPiniaPersistTypes";
+
 export * from "./types/PsrPiniaPersistTypes"
 export * from "./plugins/PsrPiniaPersist"
+
+declare module 'pinia' {
+    export interface DefineStoreOptionsBase<S extends StateTree, Store> {
+        persist?: boolean | PsrPiniaPersistTypes.Options | PsrPiniaPersistTypes.Options[]
+    }
+
+    export interface PiniaCustomProperties {
+        $restore: (opts?: { runHooks?: boolean }) => void
+        $persist: () => void
+    }
+}

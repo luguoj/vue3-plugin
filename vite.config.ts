05226e57bf3b2476d9a9c1e27d5e1940d8bdea1f
zhoudd <328704089@qq.com>
2022-12-09 17:50:15 +0800

    build: 除外配置修订

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'element-plus', '@element-plus/icons-vue'],
+      external: ['vue'],
       output: {
         globals: {
           vue: 'Vue'

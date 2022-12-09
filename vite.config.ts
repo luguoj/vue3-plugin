2d00dc6a7e684b904b5f26c03f8fb8bb9ca60035
zhoudd <328704089@qq.com>
2022-12-09 16:59:35 +0800

    build: vite库模式配置，npm发布配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue(), dts({
+        exclude: ["example/**/*.ts", "example/**/*.vue"],
+    })],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'PsrTypescriptUtils',
+            fileName: 'index'
+        },
+        rollupOptions: {
+            // 确保外部化处理那些你不想打包进库的依赖
+            external: ['vue', 'element-plus', '@element-plus/icons-vue'],
+            output: {
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        }
+    },
 })

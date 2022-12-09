ed172e5dec110226745c8e605c5916259065ea60
zhoudd <328704089@qq.com>
2022-12-09 21:05:07 +0800

    build: vite库模式配置,npm发布配置

--- src/example/main.ts
+++ src/example/main.ts
@@ -2,7 +2,7 @@ import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import {createPinia} from "pinia";
-import {createPsrPiniaPersist} from "../..";
+import {createPsrPiniaPersist} from "../package";
 import {AsyncStorage} from "./asyncStorage";
 
 const app = createApp(App)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue(), dts({
+        exclude: ["example/**/*.ts", "example/**/*.vue"],
+    })],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'PsrPiniaPersist',
+            fileName: 'index'
+        },
+        rollupOptions: {
+            // 确保外部化处理那些你不想打包进库的依赖
+            external: [
+                "@psr-framework/typescript-utils",
+                "pinia",
+                "vue"
+            ],
+            output: {
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        }
+    },
 })

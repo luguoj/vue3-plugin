a5b5df4b99a026beee6b2d4fb7657402e2e4d1f9
zhoudd <328704089@qq.com>
2022-12-12 09:05:28 +0800

    build: 模块名修订

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
-            name: 'PsrDesktopLayout',
+            name: 'PsrPrimevueExtension',
             fileName: 'index'
         },
         rollupOptions: {

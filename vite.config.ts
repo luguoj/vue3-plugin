eeefaa668adb78882d5bd4c593d2ba87c027f008
zhoudd <328704089@qq.com>
2022-12-22 09:59:30 +0800

    refactor: 声明文件构建路径修改

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     ],
     build: {
         lib: {
-            entry: resolve(__dirname, 'src/package/index.ts'),
+            entry: resolve(__dirname, 'src/index.ts'),
             name: 'PsrDragResize',
             fileName: 'index'
         },

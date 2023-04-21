110f854df8bc16d24f9528d369d2122ce562d0ae
zhoudd <328704089@qq.com>
2023-04-21 14:20:04 +0800

    feat: 色彩主题监听器

---
+++ src/package/services/useColorScheme.ts
@@ -0,0 +1,18 @@
+import {getCurrentInstance, onMounted, onUnmounted, ref, Ref} from "vue";
+
+export function usePsrColorScheme(): Ref<string> {
+    let updateInt: NodeJS.Timer | undefined
+    const vm = getCurrentInstance()
+    const colorSchemeRef: Ref<string> = ref('')
+    onMounted(() => {
+        updateInt = setInterval(() => {
+            if (vm?.proxy?.$el) {
+                colorSchemeRef.value = getComputedStyle(vm?.proxy?.$el).getPropertyValue('color-scheme')
+            }
+        }, 500)
+    })
+    onUnmounted(() => {
+        clearInterval(updateInt)
+    })
+    return colorSchemeRef
+}

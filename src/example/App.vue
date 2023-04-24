44a25caffe7e4c9f9060efdd09743bef3185a892
zhoudd <328704089@qq.com>
2023-04-24 09:44:05 +0800

    feat: 天气组件1样例

--- src/example/App.vue
+++ src/example/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-
+import {PsrWeather1} from "../package"
 </script>
 
 <template>
-  <div style="height: 100%;width: 100%;">
-
-  </div>
+    <div style="height: 100%;width: 100%;">
+        <psr-weather1 style="height: 300px;width: 600px;"/>
+    </div>
 </template>
 
 <style scoped>

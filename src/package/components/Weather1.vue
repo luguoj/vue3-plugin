0c654f097b85ffdef9f0dae6b75b49280c913273
zhoudd <328704089@qq.com>
2023-04-24 10:01:03 +0800

    feat: 添加字体颜色样式绑定

--- src/package/components/Weather1.vue
+++ src/package/components/Weather1.vue
@@ -5,15 +5,35 @@
             <iframe
                     style="transform-origin: top left"
                     :style="{scale}"
-                    width="150" height="60" frameborder="0" scrolling="no" hspace="0"
-                    src="https://i.tianqi.com/?c=code&a=getcode&id=84&color=#2266b8&icon=1"
+                    width="150" height="60" frameborder="0" scrolling="no" hspace="0" allowtransparency="true"
+                    :src="`https://i.tianqi.com/?c=code&a=getcode&id=84&color=${colorStyleRef}&icon=1`"
             />
         </div>
     </div>
 </template>
 <script lang="ts" setup>
 import {vPsrResizeObserver} from "@psr-framework/vue3-plugin-utils"
-import {computed, ref} from "vue";
+import {computed, getCurrentInstance, onMounted, onUnmounted, Ref, ref} from "vue";
+
+let updateInt: NodeJS.Timer | undefined
+const vm = getCurrentInstance()
+const colorStyleRef: Ref<string> = ref('#2266b8')
+
+function update() {
+    if (vm?.proxy?.$el) {
+        colorStyleRef.value = getComputedStyle(vm?.proxy?.$el).getPropertyValue('color')
+    }
+}
+
+onMounted(() => {
+    update()
+    updateInt = setInterval(() => {
+        update()
+    }, 500)
+})
+onUnmounted(() => {
+    clearInterval(updateInt)
+})
 
 const designSize = {width: 150, height: 60}
 const rootSize = ref({...designSize})

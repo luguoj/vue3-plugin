f20ce0e1fe42ddc5b9a328a873cd1b210628ed29
zhoudd <328704089@qq.com>
2023-07-18 09:33:14 +0800

    build: 添加libcss打包配置 build: 打包输出文件配置修订

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,30 @@ import {resolve} from 'path'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
+import libCss from "vite-plugin-libcss"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts({
-    exclude: ["example/**/*.vue", "example/**/*.ts"],
-  })],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'PsrEcharts',
-      fileName: 'vue3-plugin-echarts'
-    },
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue','echarts','vue3-resize'],
-      output: {
-        globals: {
-          // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-          vue: 'Vue'
+    plugins: [
+        vue(),
+        libCss(),
+        dts({
+            exclude: ["example/**/*.vue", "example/**/*.ts"],
+        })],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'PsrEcharts',
+            fileName: 'index'
+        },
+        rollupOptions: {
+            // 确保外部化处理那些你不想打包进库的依赖
+            external: ['vue', 'echarts', 'vue3-resize'],
+            output: {
+                globals: {
+                    // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+                    vue: 'Vue'
+                }
+            }
         }
-      }
-    }
-  },
+    },
 })

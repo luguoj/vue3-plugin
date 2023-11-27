0d00d556f9caf05370d82b0d1c56110aa657803e
zhoudd <328704089@qq.com>
2023-11-27 10:46:35 +0800

    feat: 样例 黑白主题样式

--- src/example/App.vue
+++ src/example/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import Button from 'primevue/button';
 
+let theme = true;
+
+function handler() {
+  document.getElementsByTagName('html')[0].className = theme ? 'dark' : ''
+  theme = !theme
+}
 </script>
 
 <template>
   <div style="height: 100%;width: 100%;">
-
+    <Button @click="handler">abc</Button>
+    <Button @click="handler">主题</Button>
   </div>
 </template>
 

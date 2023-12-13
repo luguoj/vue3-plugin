a4243f33318a6cda3d210625fbfe395990fe7284
zhoudd <328704089@qq.com>
2023-12-13 15:51:49 +0800

    feat: 支持监听参数

--- src/package/directives/vMutationObserver.ts
+++ src/package/directives/vMutationObserver.ts
@@ -1,16 +1,30 @@
 import {Directive} from "vue";
 
-export const vPsrMutationObserver: Directive<HTMLElement & {
-    _psrMutationObserver?: MutationObserver
-}, (mutations: MutationRecord[]) => void> = {
-    mounted(el, value) {
-        const resizeObserver = new MutationObserver(mutations => {
-            value.value(mutations)
+type TargetType = HTMLElement & {
+    _PsrMutationObserver?: MutationObserver
+}
+type Handler = (mutations: MutationRecord[]) => void
+type BindingType = {
+    options?: MutationObserverInit
+    handler: Handler
+} | Handler
+export const vPsrMutationObserver: Directive<TargetType, BindingType> = {
+    mounted(el, binding) {
+        const observer = new MutationObserver(mutations => {
+            if (binding.value instanceof Function) {
+                binding.value(mutations)
+            } else if (binding.value.handler instanceof Function) {
+                binding.value.handler(mutations)
+            }
         })
-        el._psrMutationObserver = resizeObserver
-        resizeObserver.observe(el)
+        el._PsrMutationObserver = observer
+        if (binding.value instanceof Function) {
+            observer.observe(el)
+        } else {
+            observer.observe(el, binding.value.options)
+        }
     },
     unmounted(el) {
-        el._psrMutationObserver?.disconnect()
+        el._PsrMutationObserver?.disconnect()
     }
 }
--- src/package/directives/vResizeObserver.ts
+++ src/package/directives/vResizeObserver.ts
@@ -1,16 +1,30 @@
 import {Directive} from "vue";
 
-export const vPsrResizeObserver: Directive<HTMLElement & {
-    _psrResizeObserver?: ResizeObserver
-}, (entry: ResizeObserverEntry) => void> = {
-    mounted(el, value) {
-        const resizeObserver = new ResizeObserver(entries => {
-            value.value(entries[0])
+type TargetType = HTMLElement & {
+    _PsrResizeObserver?: ResizeObserver
+}
+type Handler = (entry: ResizeObserverEntry) => void
+type BindingType = {
+    options?: ResizeObserverOptions
+    handler: Handler
+} | Handler
+export const vPsrResizeObserver: Directive<TargetType, BindingType> = {
+    mounted(el, binding) {
+        const observer = new ResizeObserver(entries => {
+            if (binding.value instanceof Function) {
+                binding.value(entries[0])
+            } else if (binding.value.handler instanceof Function) {
+                binding.value.handler(entries[0])
+            }
         })
-        el._psrResizeObserver = resizeObserver;
-        resizeObserver.observe(el)
+        el._PsrResizeObserver = observer;
+        if (binding.value instanceof Function) {
+            observer.observe(el)
+        } else {
+            observer.observe(el, binding.value.options)
+        }
     },
     unmounted(el) {
-        el._psrResizeObserver?.disconnect()
+        el._PsrResizeObserver?.disconnect()
     }
 }

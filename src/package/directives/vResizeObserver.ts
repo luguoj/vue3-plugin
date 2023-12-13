85d4c886a8b1014ae855fb58a5403ef3cd029e4b
zhoudd <328704089@qq.com>
2023-12-13 15:20:03 +0800

    feat: 卸载时取消监听

--- src/package/directives/vResizeObserver.ts
+++ src/package/directives/vResizeObserver.ts
@@ -1,10 +1,16 @@
 import {Directive} from "vue";
 
-export const vPsrResizeObserver: Directive<HTMLElement, (entry: ResizeObserverEntry) => void> = {
+export const vPsrResizeObserver: Directive<HTMLElement & {
+    _psrResizeObserver?: ResizeObserver
+}, (entry: ResizeObserverEntry) => void> = {
     mounted(el, value) {
         const resizeObserver = new ResizeObserver(entries => {
             value.value(entries[0])
         })
+        el._psrResizeObserver = resizeObserver;
         resizeObserver.observe(el)
+    },
+    unmounted(el) {
+        el._psrResizeObserver?.disconnect()
     }
 }

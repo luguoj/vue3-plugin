6e7103bfe857594ad53604880314b1a7a96a80ec
zhoudd <328704089@qq.com>
2024-04-07 14:14:38 +0800

    fix: mounted后延迟500ms才获取 color-scheme feat: 支持对指定元素引用进行 color-scheme检查 feat: 默认对文档body元素进行 color-scheme检查

--- src/package/services/useColorScheme.ts
+++ src/package/services/useColorScheme.ts
@@ -1,18 +1,21 @@
 import {getCurrentInstance, onMounted, onUnmounted, ref, Ref} from "vue";
 
-export function usePsrColorScheme(): Ref<string> {
+export function usePsrColorScheme(elRef?: Ref<Element>): Ref<string> {
     let updateInt: NodeJS.Timeout | undefined
     const vm = getCurrentInstance()
     const colorSchemeRef: Ref<string> = ref('')
     onMounted(() => {
+        colorSchemeRef.value = getColorScheme(elRef?.value || vm?.proxy?.$el)
         updateInt = setInterval(() => {
-            if (vm?.proxy?.$el) {
-                colorSchemeRef.value = getComputedStyle(vm?.proxy?.$el).getPropertyValue('color-scheme')
-            }
-        }, 500)
+            colorSchemeRef.value = getColorScheme(elRef?.value || vm?.proxy?.$el)
+        }, 200)
     })
     onUnmounted(() => {
         clearInterval(updateInt)
     })
     return colorSchemeRef
+}
+
+function getColorScheme(el: Element = document.body): string {
+    return getComputedStyle(el).getPropertyValue('color-scheme')
 }

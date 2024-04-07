df7a6357ab866b8369393a37665fe0115211ad4b
zhoudd <328704089@qq.com>
2024-04-07 14:48:42 +0800

    fix: 如果组件存在多个根节点元素，则使用body

--- src/package/services/useColorScheme.ts
+++ src/package/services/useColorScheme.ts
@@ -1,19 +1,28 @@
 import {getCurrentInstance, onMounted, onUnmounted, ref, Ref} from "vue";
+import {ComponentInternalInstance} from "@vue/runtime-core";
 
 export function usePsrColorScheme(elRef?: Ref<Element>): Ref<string> {
     let updateInt: NodeJS.Timeout | undefined
     const vm = getCurrentInstance()
     const colorSchemeRef: Ref<string> = ref('')
     onMounted(() => {
-        colorSchemeRef.value = getColorScheme(elRef?.value || vm?.proxy?.$el)
+        const vmRoot = getVmRoot(vm)
+        colorSchemeRef.value = getColorScheme(elRef?.value || vmRoot)
         updateInt = setInterval(() => {
-            colorSchemeRef.value = getColorScheme(elRef?.value || vm?.proxy?.$el)
+            colorSchemeRef.value = getColorScheme(elRef?.value || vmRoot)
         }, 200)
     })
     onUnmounted(() => {
         clearInterval(updateInt)
     })
     return colorSchemeRef
+}
+
+function getVmRoot(vm: ComponentInternalInstance | null): Element | undefined {
+    const el = vm?.proxy?.$el
+    if (el instanceof Element) {
+        return el
+    }
 }
 
 function getColorScheme(el: Element = document.body): string {

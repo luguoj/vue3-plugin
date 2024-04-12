0bdb98dfa946a7932be76101cccd91c75d690a86
zhoudd <328704089@qq.com>
2024-04-12 14:09:00 +0800

    feat: table组件样例

--- src/example/App.vue
+++ src/example/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import Button from 'primevue/button';
+import PvColumn from 'primevue/column';
+import PvInput from "primevue/inputtext"
+import FilterDataTable from "../package/components/data-table/filter/FilterDataTable.vue";
+import {PsrPrmFilterDataTableContext} from "../package";
 
 let theme = true;
 
@@ -7,13 +11,43 @@ function handler() {
   document.getElementsByTagName('html')[0].className = theme ? 'dark' : ''
   theme = !theme
 }
+
+interface DataItem {
+  id: string,
+  code: string
+}
+
+const tableContext1 = PsrPrmFilterDataTableContext.create<DataItem>({
+  loadDataHandler: () => {
+    return Promise.resolve([{id: 'id', code: 'code'}])
+  },
+  defaultFilters: () => {
+    return {
+      'id': '',
+      'code': ''
+    }
+  }
+})
+tableContext1.load()
 </script>
 
 <template>
-  <div style="height: 100%;width: 100%;">
+  <div>
     <Button @click="handler">abc</Button>
     <Button @click="handler">主题</Button>
   </div>
+  <filter-data-table :table-context="tableContext1">
+    <pv-column field="id" header="ID" filterMatchMode="equals">
+      <template #filter="{filterModel,filterCallback}">
+        <pv-input v-model="filterModel.value" @change="filterCallback()"/>
+      </template>
+    </pv-column>
+    <pv-column field="code" header="CODE" filterMatchMode="equals">
+      <template #filter="{filterModel,filterCallback}">
+        <pv-input v-model="filterModel.value" @change="filterCallback()"/>
+      </template>
+    </pv-column>
+  </filter-data-table>
 </template>
 
 <style scoped>

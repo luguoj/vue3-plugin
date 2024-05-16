cead6f2a71f576f7d3ec77c4c0968a6f80270909
zhoudd <328704089@qq.com>
2024-05-16 14:01:52 +0800

    fix: 没有提供监控选项参数时报错

--- src/package/common/directives/vMutationObserver.ts
+++ src/package/common/directives/vMutationObserver.ts
@@ -5,24 +5,16 @@ type TargetType = HTMLElement & {
 }
 type Handler = (mutations: MutationRecord[]) => void
 type BindingType = {
-    options?: MutationObserverInit
+    options: MutationObserverInit
     handler: Handler
-} | Handler
+}
 export const vPsrMutationObserver: Directive<TargetType, BindingType> = {
     mounted(el, binding) {
         const observer = new MutationObserver(mutations => {
-            if (binding.value instanceof Function) {
-                binding.value(mutations)
-            } else if (binding.value.handler instanceof Function) {
-                binding.value.handler(mutations)
-            }
+            binding.value.handler(mutations)
         })
         el._PsrMutationObserver = observer
-        if (binding.value instanceof Function) {
-            observer.observe(el)
-        } else {
-            observer.observe(el, binding.value.options)
-        }
+        observer.observe(el, binding.value.options)
     },
     unmounted(el) {
         el._PsrMutationObserver?.disconnect()

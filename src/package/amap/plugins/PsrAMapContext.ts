592fdcdd88bb3ee82471db53d5c4b3aa999f9ab8
zhoudd <328704089@qq.com>
2024-05-23 15:25:34 +0800

    feat: 第一次使用地图组件时才加载API

--- src/package/amap/plugins/PsrAMapContext.ts
+++ src/package/amap/plugins/PsrAMapContext.ts
@@ -1,4 +1,3 @@
-import AMapLoader from "@amap/amap-jsapi-loader";
 import {PsrAMapTypes} from "../types/PsrAMapTypes.ts";
 import {
     App,
@@ -17,10 +16,11 @@ const injectKey = 'psr-a-map'
 
 export class PsrAMapContext {
     private static _activeInstance: PsrAMapContext
-    private readonly ready: Promise<any>
+    private readonly options: PsrAMapTypes.Options
+    private ready: Promise<any> | undefined
 
     private constructor(options: PsrAMapTypes.Options) {
-        this.ready = AMapLoader.load(options)
+        this.options = options
     }
 
     static create(options: PsrAMapTypes.Options) {
@@ -59,23 +59,35 @@ export class PsrAMapContext {
         // 组件装载时，创建容器监听器，选项监听器，创建地图实例
         let containerWatcher: WatchStopHandle | undefined = undefined
         let optionsWatcher: WatchStopHandle | undefined = undefined
+
+        function init(AMap: any) {
+            containerWatcher = watch(containerDivRef, (containerDiv, old) => {
+                if (old != containerDiv) {
+                    dispose()
+                }
+                if (containerDiv) {
+                    build(AMap, containerDiv, optionsRef.value)
+                }
+            }, {immediate: true})
+            optionsWatcher = watch(optionsRef, options => {
+                // 如果视图模式变更，需要重新初始化地图实例
+                if (options?.viewMode && options.viewMode != viewMode && containerDivRef.value) {
+                    dispose()
+                    build(AMap, containerDivRef.value, options)
+                }
+            }, {deep: true, immediate: true})
+        }
+
         onMounted(() => {
-            this.getInstance().ready.then((AMap) => {
-                containerWatcher = watch(containerDivRef, (containerDiv, old) => {
-                    if (old != containerDiv) {
-                        dispose()
-                    }
-                    if (containerDiv) {
-                        build(AMap, containerDiv, optionsRef.value)
-                    }
-                }, {immediate: true})
-                optionsWatcher = watch(optionsRef, options => {
-                    // 如果视图模式变更，需要重新初始化地图实例
-                    if (options?.viewMode && options.viewMode != viewMode && containerDivRef.value) {
-                        dispose()
-                        build(AMap, containerDivRef.value, options)
-                    }
-                }, {deep: true, immediate: true})
+            const instance = this.getInstance()
+            if (!instance.ready) {
+                instance.ready = import("@amap/amap-jsapi-loader")
+                    .then((AMapLoader) => {
+                        return AMapLoader.load(instance.options)
+                    })
+            }
+            instance.ready.then((AMap) => {
+                init(AMap)
             })
         })
         // 组件卸载时取消监听器，并销毁地图实例

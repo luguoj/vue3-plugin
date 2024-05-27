ae6a7cd4fb8786527c2d9908d5819f58e1c58562
zhoudd <328704089@qq.com>
2024-05-27 11:17:44 +0800

    docs: 图标标记点文档样例

---
+++ docs/reference/amap/components/marker/IconUseAnchor.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {PsrAMap, PsrAMapMarker, PsrAMapTypes} from "@psr-framework/vue3-plugin";
+import {ref} from "vue";
+
+const pos = ref<PsrAMapTypes.LngLat>({lng: 116.397428, lat: 39.90923})
+</script>
+
+<template>
+  <div style="height: 400px;">
+    <psr-a-map
+        style="height: 100%;"
+        :map-center="{lng:116.397428,lat:39.90923}"
+    >
+      <template #default="{map}">
+        <psr-a-map-marker
+            :a-map="map"
+            v-model:marker-position="pos"
+            marker-icon="https://a.amap.com/jsapi_demos/static/demo-center/icons/poi-marker-default.png"
+            marker-anchor="bottom-center"
+            marker-draggable
+        />
+      </template>
+    </psr-a-map>
+  </div>
+</template>
--- docs/reference/amap/components/marker/index.md
+++ docs/reference/amap/components/marker/index.md
@@ -6,3 +6,29 @@
 demo-preview=./Default.vue
 :::
 
+## 图标点标记
+
+### 使用anchor定位图标
+  
+marker-anchor的可选值 : 
+- top-left'
+- top-center
+- top-right
+- middle-left
+- center
+- middle-right
+- bottom-left
+- bottom-center
+- bottom-right
+
+:::preview
+demo-preview=./IconUseAnchor.vue
+:::
+
+### 使用offset定位图标
+
+如果指定了anchor，将优先使用anchor。如果想使用图片内部的位置作为锚点，可使用offset。
+
+:::preview
+demo-preview=./IconUseOffset.vue
+:::

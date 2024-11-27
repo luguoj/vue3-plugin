201c61aefb41ef00f5f3ae1ce06ec9e87d6b38bb
zhoudd <328704089@qq.com>
2024-11-27 08:22:24 +0800

    feat: 取消svg渲染模式

--- src/package/antv-g6/services/graph/useGraph.ts
+++ src/package/antv-g6/services/graph/useGraph.ts
@@ -1,6 +1,5 @@
 import {Ref, ShallowRef, shallowRef, toRaw, watch} from "vue";
 import {Graph, GraphOptions} from "@antv/g6";
-import { Renderer as SVGRenderer } from '@antv/g-svg';
 export function useGraph(
     containerDivRef: Ref<HTMLDivElement | undefined>,
     options?: {
@@ -11,8 +10,7 @@ export function useGraph(
         if (containerDiv) {
             graphRef.value = new Graph({
                 ...options?.graph,
-                container: toRaw(containerDivRef.value),
-                renderer: () => new SVGRenderer()
+                container: toRaw(containerDivRef.value)
             })
             graphRef.value.render().then()
         }
